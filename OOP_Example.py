d46c4b7e002c0a3b933f6e80b300cd27b1708146
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-10-01 12:35:33 -0400

    added a README.md file.

--- OOP_Example.py
+++ OOP_Example.py
@@ -1,4 +1,9 @@
-class PlayerCharacter:
+""" The class below is a constructor class. The idea is that the class
+acts as a blueprint how the program will set the parameters for the parent
+class and their inherited class. Note: There are no parent class or inherited
+class in this example. There are only the constructor class and a basic call 
+of the class function."""
+class PlayerCharacter:  
     membership = True
     def __init__(self, name, age):
         if self.membership:
@@ -8,13 +13,17 @@ class PlayerCharacter:
     def shout(self):
         print(f'my name is {self.name}')
 
+# The below is the instantiation of the class PlayerCharacter.
 player1 = PlayerCharacter('Mustafa', 28)
 player2 = PlayerCharacter('Sabina', 21)
 
+# The output of different players instantiated from above.
 print(player1.name, player1.age, player1.shout())
 print(f'{player1.name} is {player1.age}. {player1.shout()}.')
 
 
 print(f'{player2.name} is {player2.age}. {player2.shout()}')
 
-# help(PlayerCharacter)
+""" This call function and method calls the blueprint of the class.
+This help() function can call anytthing on the parameter."""
+help(PlayerCharacter)
---
+++ README.md
@@ -0,0 +1,9 @@
+This program is considered a practice example of Object Orineted Programming for Python.
+The idea is that this is to be used as a reference for creating a constructor class. There will be further 
+examples on other OOP.py files that will be created to create robustt refernce with more ways to create 
+a robust program.
+
+There are some notes to consider:
+
+help(): This call function and method calls the blueprint of the class.
+This help() function can call anytthing on the parameter.
